907d827d4291d84bc92a463a243fba77a9a31165
eunpyo <eunpyo@umich.edu>
2019-07-10 17:02:07 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,73 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const port = 3000;
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+
+});
+
+app.post("/", function(req, res) {
+  var firstName = req.body.fname;
+  var lastName = req.body.lname;
+  var email = req.body.email;
+
+  var data={
+    members: [
+      {
+        email_address: email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: lastName
+      }
+    }
+    ]
+  };
+  var jsonData = JSON.stringify(data);
+
+  var options = {
+    url: "https://us3.api.mailchimp.com/3.0/lists/2523c24068",
+    method: "POST",
+    headers: {
+      "Authorization": "jason1 8791565d173f1b30feefc131844057d1-us3"
+    },
+    body: jsonData
+  };
+  request(options, function(error, response, body) {
+    if(error){
+      res.sendFile(__dirname + "/failure.html");
+    }
+    else{
+      if(response.statusCode == 200){
+        res.sendFile(__dirname + "/success.html");
+      }
+      else{
+        res.sendFile(__dirname + "/failure.html");
+      }
+    }
+
+  });
+});
+
+app.post("/failure", function(req, res){
+  res.redirect("/");
+});
+
+
+//process.env.PORT is what heroku places for dynamically located port
+app.listen(process.env.PORT || 3000, function() {
+  console.log("server is running on port 3000");
+});
+//API  key
+//8791565d173f1b30feefc131844057d1-us3
+//unique id for audience
+//2523c24068
